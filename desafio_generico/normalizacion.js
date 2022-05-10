d1a49f947e7ad2e512a26e55ee245f13e764986a
derapalne <derapalne@gmail.com>
2022-05-10 03:13:39 -0300

    Arrancando genérico

---
+++ desafio_generico/normalizacion.js
@@ -0,0 +1,8 @@
+import normalizr from 'normalizr';
+const normalize = normalizr.normalize;
+const denormalize = normalizr.denormalize;
+const schema = normalizr.schema;
+
+import originalData from './holding.js';
+
+console.log(originalData);
